64f0c958b2b029c382667819eec84280a8ee8061
Dinesh A. Joshi <djoshi@yahoo-inc.com>
2014-01-18 00:33:26 -0800

    Initial Commit

---
+++ TodoListApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TodoListApp
+//
+//  Created by Dinesh Joshi on 1/18/14.
+//  Copyright (c) 2014 Dinesh Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoListApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TodoListApp
+//
+//  Created by Dinesh Joshi on 1/18/14.
+//  Copyright (c) 2014 Dinesh Joshi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
